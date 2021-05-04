1479ec858003bb610f4fb77fef452aa7d06490a7
Aishwarya <aishuharish.work@gmail.com>
2021-05-04 12:59:13 -0400

    item fie added

--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -1,37 +1,8 @@
 import React from "react";
+import Item from "./Item";
 
 const InventoryList = ({ cars }) => {
-  const renderedList = cars.map((car) => {
-    const url = require(`../images/${car.id}_${car.make}_${car.model}.png`)
-      .default;
-
-    const retailPrice =
-      parseInt(car.basePrice) +
-      parseInt(car.features.doors === 4 ? 2500.0 : 0.0) +
-      parseInt(
-        car.features.fuel === "Gas"
-          ? 0.0
-          : car.features.fuel === "Hybrid"
-          ? 10000.0
-          : 15000.0
-      ) +
-      parseInt(car.features.transmission === "Automatic" ? 1000.0 : 0.0) +
-      parseInt(car.features.interior === "Cloth" ? 0.0 : 1500.0);
-    //console.log(retailPrice);
-
-    return (
-      <div className="card" key={car.id}>
-        <div className="image">
-          <img src={url} alt="carImage" />
-        </div>
-        <div className="content">
-          <p className="header">{car.make}</p>
-          <p>Model: {car.model}</p>
-          <p>Retail Price: {retailPrice}</p>
-        </div>
-      </div>
-    );
-  });
+  const renderedList = cars.map((car) => <Item car={car} />);
 
   return (
     <div>
---
+++ src/components/Item.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Item = ({ car }) => {
+  const url = require(`../images/${car.id}_${car.make}_${car.model}.png`)
+    .default;
+
+  const retailPrice =
+    parseInt(car.basePrice) +
+    parseInt(car.features.doors === 4 ? 2500.0 : 0.0) +
+    parseInt(
+      car.features.fuel === "Gas"
+        ? 0.0
+        : car.features.fuel === "Hybrid"
+        ? 10000.0
+        : 15000.0
+    ) +
+    parseInt(car.features.transmission === "Automatic" ? 1000.0 : 0.0) +
+    parseInt(car.features.interior === "Cloth" ? 0.0 : 1500.0);
+  //console.log(retailPrice);
+
+  return (
+    <div className="card" key={car.id}>
+      <div className="image">
+        <img src={url} alt="carImage" />
+      </div>
+      <div className="content">
+        <p className="header">{car.make}</p>
+        <p>Model: {car.model}</p>
+        <p>Retail Price: {retailPrice}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
